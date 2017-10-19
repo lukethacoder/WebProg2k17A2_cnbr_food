3a735150d305b8a81e14a939f5d9e3de642ea9bc
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2017-10-19 12:48:09 +1100

    commit

---
+++ canberra_food_a2/Models/Rest_reviews.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace canberra_food_a2.Models
+{
+    public class Rest_reviews
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+    }
+}
