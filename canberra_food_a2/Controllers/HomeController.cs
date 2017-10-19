00d6935bccb94f2ed1c98bc14251830441485362
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2017-10-19 12:41:25 +1100

    Initial commit

---
+++ canberra_food_a2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace canberra_food_a2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cuisine()
+        {
+            ViewData["Message"] = "This is your Cuisine page";
+
+            return View();
+        }
+
+        public IActionResult Dishes()
+        {
+            ViewData["Message"] = "This is your Cuisine page";
+
+            return View();
+        }
+        public IActionResult Location()
+        {
+            ViewData["Message"] = "This is your Location page";
+
+            return View();
+        }
+        public IActionResult Price()
+        {
+            ViewData["Message"] = "This is your Price page";
+
+            return View();
+        }
+        public IActionResult Restaurants()
+        {
+            ViewData["Message"] = "This is your Restaurants page";
+
+            return View();
+        }
+        public IActionResult Reviews()
+        {
+            ViewData["Message"] = "This is your Reviews page";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ canberra_food_a2/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace canberra_food_a2.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
